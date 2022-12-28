a96ebee7b0586fa317f6407ff548dfb12f094324
sericaer <sericaer@outlook.com>
2022-12-28 17:21:06 +0800

    Add MoveControlRegister

---
+++ Assets/Plugins/MyExtentions/MoveControl/MoveControlResigter.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+public static class MoveControlResigter
+{
+    [MenuItem("GameObject/UI/MyExtentions/MoveControlResigter")]
+    public static void AddDialogPanel(MenuCommand menuCommand)
+    {
+        var parent = menuCommand.context as GameObject;
+        var instance = Object.Instantiate(Resources.Load("MoveControl"), parent.transform) as GameObject;
+        instance.name = "MoveControl";
+    }
+}
+
+#endif
