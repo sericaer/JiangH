0d205a8e6a125fff9a9a76aa52c34599b4328260
sericaer <sericaer@outlook.com>
2023-01-03 17:00:07 +0800

    Add Plugins/MyExtentions/DialogPanel

---
+++ Assets/Plugins/MyExtentions/DialogPanel/DialogPanelResigter.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+public static class DialogPanelResigter
+{
+    [MenuItem("GameObject/UI/MyExtentions/DialogPanel")]
+    public static void AddDialogPanel(MenuCommand menuCommand)
+    {
+        var parent = menuCommand.context as GameObject;
+        var instance = Object.Instantiate(Resources.Load("DialogPanel"), parent.transform) as GameObject;
+        instance.name = "DialogPanel";
+    }
+}
+
+#endif
